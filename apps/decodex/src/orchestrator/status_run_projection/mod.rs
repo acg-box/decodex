9092540bde21e246ddf75157d6f3522dbf2b30cb
XV <xv@acg.box>
2026-06-30 08:20:33 -0400

    {"schema":"decodex/commit/2","change":"{\"schema\":\"decodex/commit/1\",\"summary\":\"Modularize status run projection\",\"authority\":\"XY-1115\"}","authority":"manual","impact":"compatible"}

---
+++ apps/decodex/src/orchestrator/status_run_projection/mod.rs
@@ -0,0 +1,44 @@
+//! Operator run status projection, protocol/activity readback, and lane lifecycle metrics.
+
+use super::{
+	ARCHITECTURE_RECOVERY_PACKET_EVENT_TYPE, ARCHITECTURE_RECOVERY_STARTED_EVENT_TYPE,
+	ARCHITECTURE_RECOVERY_TERMINAL_EVENT_TYPE, AUTHORITY_BOUNDARY_CHECK_EVENT_TYPE,
+	AUTHORITY_DECISION_REQUEST_EVENT_TYPE, AgentPrivateEvidenceRef,
+	CONTINUATION_PENDING_RUN_STATUS, ChildAgentActivityBucket, ChildAgentActivitySummary,
+	CodexAccountActivitySummary, Duration, EXECUTION_LIVENESS_PROCESS_IDENTITY_MISMATCH, HashMap,
+	HashSet, OperatorArchitectureRecoveryStatus, OperatorAuthorityDecisionRequestStatus,
+	OperatorBoundaryStatus, OperatorContinuationRecoveryStatus, OperatorHistoryLaneStatus,
+	OperatorLaneControlProjection, OperatorLaneLifecycleAttemptEvidence,
+	OperatorLaneLifecycleMetrics, OperatorLaneLifecyclePhaseMetrics, OperatorLifecycleMetricPhase,
+	OperatorLoopStatus, OperatorPhaseAcceptanceStatus, OperatorRecoveryBudgetStatus,
+	OperatorReviewCheckpointStatus, OperatorReviewCheckpointSummaryFields,
+	OperatorReviewLoopStatus, OperatorReviewRouteCount, OperatorRunAppServerState,
+	OperatorRunControlCapability, OperatorRunLifecycleProjection, OperatorRunProtocolSummary,
+	OperatorRunStatus, OperatorRunTiming, OperatorTerminalFinalizeProjection,
+	PHASE_ACCEPTANCE_CHECK_EVENT_TYPE, PHASE_GOAL_RECOVERY_AUTOMATIC_CONTINUATION_LIMIT,
+	PHASE_GOAL_RECOVERY_BLOCKED_EVENT_TYPE, PHASE_GOAL_RECOVERY_EVENT_TYPE, PrivateExecutionEvent,
+	ProjectLoopEvidenceSnapshot, ProjectRunStatus, ProtocolActivitySummary,
+	REVIEW_POLICY_CONVERGENCE_BUDGET, RUN_LEASE_IDLE_TIMEOUT, RUN_OPERATION_AGENT_RUN,
+	RUN_OPERATION_IDLE, RUN_OPERATION_REVIEW_WRITEBACK, RUN_OPERATION_WAITING_EXTERNAL,
+	ReviewLevel, Rfc3339, RunActivityMarker, ServiceConfig, StateStore,
+	TERMINAL_GUARDED_RUN_STATUS, Value, append_primary_account_if_missing,
+	marker_process_liveness_for_marker, not_loaded_history_ledger_outcome, observed_idle_duration,
+	operator_authority_decision_request_status_from_event, operator_autonomy_lineage_statuses,
+	operator_autonomy_objective_status, operator_autonomy_proposal_statuses,
+	operator_autonomy_report_status, operator_autonomy_signal_statuses,
+	operator_run_counts_as_attention, operator_run_counts_as_running,
+	operator_run_has_fresh_execution, operator_run_has_recent_app_server_execution,
+	operator_run_needs_attention, private_evidence_ref_for_run_fields, public_text,
+	relative_worktree_path_for_path, run_activity_idle_timeout, state,
+};
+use time::OffsetDateTime;
+
+mod history;
+mod loop_status;
+mod run;
+mod runtime;
+
+#[allow(clippy::wildcard_imports)] pub(super) use history::*;
+#[allow(clippy::wildcard_imports)] pub(super) use loop_status::*;
+#[allow(clippy::wildcard_imports)] pub(super) use run::*;
+#[allow(clippy::wildcard_imports)] pub(super) use runtime::*;
