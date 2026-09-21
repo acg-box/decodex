bb5b73b14449f73c9fccc84de7bec01aa493fa95
XV <xv@acg.box>
2026-09-21 07:39:00 -0400

    Persist immutable live reviewer dispatch and outcome records

--- database/src/lib.rs
+++ database/src/lib.rs
@@ -7,6 +7,8 @@ mod accounts;
 mod chief;
 mod chief_app_settings;
 pub use chief_app_settings::ChiefAppSettingsAttempt;
+mod chief_live_settings;
+pub use chief_live_settings::{ChiefLiveReviewerAttempt, ChiefLiveReviewerReceipt};
 mod chief_auth_recovery;
 pub use chief_auth_recovery::ChiefAuthRecoveryObservation;
 mod chief_install;
